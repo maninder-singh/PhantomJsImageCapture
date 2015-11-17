13e64a7008a000f3b7d9a1603eb437cdd747b913
Maninder Singh <nsit.maninder@gmail.com>
2015-11-17 15:41:36 +0530

    remove hardcoded path of js and save image file

--- grails-app/services/com/sample/phantomjs/ImageService.groovy
+++ grails-app/services/com/sample/phantomjs/ImageService.groovy
@@ -7,11 +7,15 @@ import javax.imageio.ImageIO
 @Transactional
 class ImageService {
 
-    def getWebPageAsImage() {
+    def getWebPageAsImage(def webPageUrl) {
 
-        def phantomJsShell = "phantomjs /home/maninders/source_code/grails/PhantomJsImageCapture/grails-app/assets/javascripts/image-capture.js http://localhost:8080/PhantomJsImageCapture/webpage";
+        def filePath = this.class.classLoader.getResource("image_capture").getPath().toString()
+        def jsFilePath = filePath + "/javascripts/image-capture.js"
+        def screenshotPath = filePath + "/images/capture_webpage.png"
+        def phantomJsShell = "phantomjs ${jsFilePath} ${webPageUrl} ${screenshotPath}"
         phantomJsShell.execute()
-        def image = ImageIO.read(new File("/home/maninders/source_code/grails/PhantomJsImageCapture/grails-app/assets/javascripts/demo1.png"))
+        sleep(4000)
+        def image = ImageIO.read(new File(screenshotPath))
         def imageWriter = new ByteArrayOutputStream()
         ImageIO.write(image,"png",imageWriter)
         def imageBytes = imageWriter.toByteArray()
