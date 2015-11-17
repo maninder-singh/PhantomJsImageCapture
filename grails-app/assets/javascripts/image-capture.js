3e87500f470a39aa3ec884a478ef23efcbaafe49
Maninder Singh <nsit.maninder@gmail.com>
2015-11-17 16:45:38 +0530

    move assets/javascript code to grails-app/conf/image_capture/

--- grails-app/assets/javascripts/image-capture.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-var system = require("system");
-var args = system.args;
-page.open("https://www.google.co.in", function () {
-    page.render('demo1.png');
-    phantom.exit();
-});
