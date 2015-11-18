c148353d1e7c57df683dd8a776d58fd03ccc111f
Maninder Singh <nsit.maninder@gmail.com>
2015-11-18 12:16:30 +0530

    Convert byte[] to Base64 dataurl

--- grails-app/controllers/com/sample/phantomjs/ImageController.groovy
+++ grails-app/controllers/com/sample/phantomjs/ImageController.groovy
@@ -2,7 +2,7 @@ package com.sample.phantomjs
 
 import grails.converters.JSON
 import org.apache.commons.codec.binary.Base64
-
+import org.apache.commons.codec.binary.StringUtils
 class ImageController {
 
     ImageService imageService
@@ -15,7 +15,11 @@ class ImageController {
         def webPageUrl = data["webPageUrl"] as String
         def result = [:]
         def imageData = imageService.getWebPageAsImage(webPageUrl)
-        result.put("data","data:image/png;base64," + new String(Base64.encodeBase64(imageData)))
+//        result.put("data","data:image/png;base64," + new String(Base64.encodeBase64(imageData)))
+        StringBuilder sb = new StringBuilder()
+        sb.append("data:image/png;base64,")
+        sb.append(StringUtils.newStringUtf8(Base64.encodeBase64(imageData,false)))
+        result.put("data",sb.toString())
         render result as JSON
     }
 }
